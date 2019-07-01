5ba513e49fec9b6dd7ee43ad81fc14bb3aac25a7
Giuseppe Sapienza <giuseppe.sapienza@xcoding.it>
2019-07-01 11:00:08 +0200

    Initial Commit

---
+++ State/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  State
+//
+//  Created by Giuseppe Sapienza on 01/07/2019.
+//  Copyright © 2019 Giuseppe Sapienza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
