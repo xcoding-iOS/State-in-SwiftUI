ef23f5dec6ad0c6c98d52748cf259ae681731360
Giuseppe Sapienza <giuseppe.sapienza@xcoding.it>
2019-07-05 12:24:32 +0200

    completed

--- State/ContentView.swift
+++ State/ContentView.swift
@@ -9,15 +9,59 @@
 import SwiftUI
 
 struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+    
+    @State var priceText: String = ""
+    @State var vatValue: Int = 22
+    
+    var priceNumber: Double? {
+        Double(priceText)
     }
+    
+    var resultPrice: Double? {
+        guard let price = priceNumber else {
+            return nil
+        }
+        
+        let result = price + (price * Double(vatValue)/100)
+        return result
+    }
+    
+    var resultText: String {
+        String(format: "%.2f", resultPrice ?? 0)
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("VAT Calculator")
+                .font(.title)
+            
+            TextField($priceText, placeholder: Text("Inserisci il prezzo"))
+                .textFieldStyle(.roundedBorder)
+                .border(priceNumber == nil ? Color.red : Color.green)
+            
+            Stepper(value: $vatValue, in: 0...100) {
+                Text("VAT Value: \(vatValue) %")
+            }
+            
+            if resultPrice != nil {
+                Text("Result: ")
+                +
+                Text(resultText)
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+    
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(priceText: "123")
     }
 }
 #endif
